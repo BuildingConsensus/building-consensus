a560ac23b957d904f432dde93e2399294a780b66
Boris <bbnedev@gmail.com>
2025-04-01 20:11:06 +0000

    Added react-router-dom and setup basic page routing

---
+++ src/pages/EstateServices/index.jsx
@@ -0,0 +1,9 @@
+
+export function EstateServices() {
+  return (
+    <div>
+      <h1>Estate Services</h1>
+      <p>Hello Estates</p>
+    </div>
+  )
+}
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,9 @@
+
+export function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Hello World</p>
+    </div>
+  )
+}
