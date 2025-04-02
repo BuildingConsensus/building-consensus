0504e3f7d4668123fb28c56e7932877369636f14
Boris <bbnedev@gmail.com>
2025-04-02 00:47:31 +0000

    changed the structure of Layout.jsx and added the sunset

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,13 +1,21 @@
+// You may place things outside of the Router in this file in order to prevent re-renders
 import { Navigationbar } from "./components/Navbar";
+import { Sunset } from "./components/Sunset";
 import { Outlet } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export function Layout() {
   return (
-    <>
+    <Container>
       <Navigationbar />
-      <main>
-        <Outlet />
-      </main>
-    </>
+      <Row>
+        <Col><Sunset /></Col>
+      </Row>
+      <Row>
+        <Col><Outlet /></Col>
+      </Row>
+    </Container>
   );
 }
