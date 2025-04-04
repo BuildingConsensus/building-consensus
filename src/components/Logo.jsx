0277f31d66355974197e81c4cb07920576f8573e
Boris <bbnedev@gmail.com>
2025-04-04 01:14:59 +0000

    Added content to the home screen and altered the css.

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,10 @@
 import Image from 'react-bootstrap/Image';
 import sunset from '../assets/sunset cropped.jpg';
-import './Logo.css'
 
 export function Logo() {
   return (
-    <Image src={sunset} alt="Sunset" className="sunset-image" fluid />
+    <>
+      <Image src={sunset} alt="Sunset" className="sunset-image" fluid />
+    </>
   );
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,13 @@
+import { Container, Row } from 'react-bootstrap'
+import './Home.css'
 
 export function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <p>Hello World</p>
-    </div>
+    <Container className="home-container">
+        <h2 className="tagline">
+          <span className='accent1'>Access</span> to professional dispute resolution
+          <br />has never been <span className='accent1'>so easy.</span> </h2>
+        <p className="description">Avoid headaches while saving money and time by letting us mediate instead of the courts.</p>
+    </Container>
   )
 }
