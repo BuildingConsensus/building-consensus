b2252ae6270e93ac96c491b2d11c2a2bf6e5c8b7
Boris <bbnedev@gmail.com>
2025-04-04 17:41:21 +0000

    Made the links on the cards buttons instead

--- src/pages/Home/Card.jsx
+++ src/pages/Home/Card.jsx
@@ -1,5 +1,6 @@
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 import './Card.css';
 
 export function CardComponent({ title, description, imageSrc, link }) {
@@ -8,9 +9,9 @@ export function CardComponent({ title, description, imageSrc, link }) {
       <Card.Body>
         <Card.Title className='mb-3 accent2'>{title}</Card.Title>
         <Card.Text className='text-muted'>{description}</Card.Text>
-        <Card.Link as={Link} to={link} className='stretched-link'>Card Link</Card.Link>
+        <Card.Img className='mb-4' src={imageSrc} />
+        <Button as={Link} to={link} variant="primary" className='card-button stretched-link'>Learn more</Button>
       </Card.Body>
-      <Card.Img src={imageSrc} />
     </Card>
   );
 }
