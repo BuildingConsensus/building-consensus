4bee3495f487cd025cd09c99d732b8416987eb6d
Boris <bbnedev@gmail.com>
2025-04-07 16:52:41 +0000

    chore: changed colors to work with bootstrap and made the homepage look like the figma.

--- src/pages/Home/Card.jsx
+++ src/pages/Home/Card.jsx
@@ -3,13 +3,12 @@ import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import './Card.css';
 
-export function CardComponent({ title, description, imageSrc, link }) {
+export function CardComponent({ title, description, link }) {
   return (
     <Card style={{ width: '20rem' }} className='card'>
       <Card.Body>
-        <Card.Title className='mb-3 accent2'>{title}</Card.Title>
+        <Card.Title className='mb-3 text-center'>{title}</Card.Title>
         <Card.Text className='text-muted'>{description}</Card.Text>
-        <Card.Img className='mb-4' src={imageSrc} />
         <Button as={Link} to={link} variant="primary" className='card-button stretched-link'>Learn more</Button>
       </Card.Body>
     </Card>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,25 +5,22 @@ import './Home.css'
 export function Home() {
   return (
     <Container className="home-container">
-        <h2 className="tagline">Building Consensus</h2>
-        <p className="description text-muted">Avoid headaches while saving money and time by letting us mediate instead of the courts.</p>
+        <h2 className="company-name">Building Consensus</h2>
+        <p className="description">Mediations, Civil Disputes, <br/> and Elections Appeals</p>
         <Row className='card-row'>
           <CardComponent
             title="Indigenous Election Appeals"
-            description="A neutral third party helps disputing parties reach a mutually acceptable agreement."
-            imageSrc="https://via.placeholder.com/150"
+            description="Resolve band and council elections appeals in 6 months or less."
             link="/ElectionAppeals"
           />
           <CardComponent
             title="Family Mediation"
-            description="A neutral third party makes a binding decision to resolve the dispute."
-            imageSrc="https://via.placeholder.com/150"
+            description="Family law and court ordered mediations."
             link="/FamilyMediation"
           />
           <CardComponent
             title="Employment Mediation"
-            description="Parties communicate directly to reach a mutually acceptable agreement."
-            imageSrc="https://via.placeholder.com/150"
+            description="Resolve workplace disputes and grievances."
             link="/EmploymentMediation"
           />
         </Row>
