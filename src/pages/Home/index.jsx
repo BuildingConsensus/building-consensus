d29043ea364618d694cb52cc33a733f2ffeb7b08
Boris <bbnedev@gmail.com>
2025-04-07 23:22:37 +0000

    chore: cleaned up the html and added brown background to the employee section.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,35 +6,31 @@ import './Home.css'
 
 export function Home() {
   return (
-    <>
-      <Container className="home-container">
-        <h2 className="company-name">Building Consensus</h2>
-        <p className="description">Mediations, Civil Disputes, <br/> and Elections Appeals</p>
-        <Row className='centered mb-3'>
-          <CardComponent
-            title="Indigenous Election Appeals"
-            description="Resolve band and council elections appeals in 6 months or less."
-            link="/ElectionAppeals"
-          />
-          <CardComponent
-            title="Family Mediation"
-            description="Family law and court ordered mediations."
-            link="/FamilyMediation"
-          />
-          <CardComponent
-            title="Employment Mediation"
-            description="Resolve workplace disputes and grievances."
-            link="/EmploymentMediation"
-          />
-        </Row>
-      </Container>
-      <Container fluid className='py-2'>
-        <Row className='portraits flex-nowrap'>
-          <EmployeeCardComponent name="Marilyn Adsit" phone="(780) 123-4567" email="adsit@outlook.ca" srcImg={"https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}/>
-          <EmployeeCardComponent name="James H." phone="(780) 123-4567" email="james@outlook.ca" srcImg={"https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}/>
-          <EmployeeCardComponent name="Boris Nedev" phone="(780) 123-4567" email="boris@outlook.ca" srcImg={"https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}/>
-        </Row>
-      </Container>
-    </>
+    <Container className="home-container">
+      <h2 className="company-name">Building Consensus</h2>
+      <p className="description">Mediations, Civil Disputes, <br/> and Elections Appeals</p>
+      <Row className='centered mb-3'>
+        <CardComponent
+          title="Indigenous Election Appeals"
+          description="Resolve band and council elections appeals in 6 months or less."
+          link="/ElectionAppeals"
+        />
+        <CardComponent
+          title="Family Mediation"
+          description="Family law and court ordered mediations."
+          link="/FamilyMediation"
+        />
+        <CardComponent
+          title="Employment Mediation"
+          description="Resolve workplace disputes and grievances."
+          link="/EmploymentMediation"
+        />
+      </Row>
+      <Row className='portraits flex-nowrap employee-row'>
+        <EmployeeCardComponent name="Marilyn Adsit" phone="(780) 123-4567" email="adsit@outlook.ca" srcImg={"https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}/>
+        <EmployeeCardComponent name="James H." phone="(780) 123-4567" email="james@outlook.ca" srcImg={"https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}/>
+        <EmployeeCardComponent name="Boris Nedev" phone="(780) 123-4567" email="boris@outlook.ca" srcImg={"https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}/>
+      </Row>
+    </Container>
   )
 }
