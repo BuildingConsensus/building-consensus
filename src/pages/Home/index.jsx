39559719aaf56071cdf8eee551f70434ff8e0079
jjadsit <adsit@ualberta.ca>
2025-04-07 15:38:37 -0600

    feat: portraits now scroll in x-axis

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,7 +10,7 @@ export function Home() {
       <Container className="home-container">
         <h2 className="company-name">Building Consensus</h2>
         <p className="description">Mediations, Civil Disputes, <br/> and Elections Appeals</p>
-        <Row className='card-row mb-3'>
+        <Row className='centered mb-3'>
           <CardComponent
             title="Indigenous Election Appeals"
             description="Resolve band and council elections appeals in 6 months or less."
@@ -29,13 +29,10 @@ export function Home() {
         </Row>
       </Container>
       <Container fluid className='py-2'>
-        <Row className='card-row mb-3 flex-nowrap d-flex'>
+        <Row className='scroll-x flex-nowrap'>
           <EmployeeCardComponent name="Marilyn Adsit" phone="(780) 123-4567" email="adsit@outlook.ca" srcImg={"https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}/>
           <EmployeeCardComponent name="James H." phone="(780) 123-4567" email="james@outlook.ca" srcImg={"https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}/>
-          <EmployeeCardComponent name="Marilyn Adsit" phone="(780) 123-4567" email="adsit@outlook.ca" srcImg={"https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}/>
-          <EmployeeCardComponent name="James H." phone="(780) 123-4567" email="james@outlook.ca" srcImg={"https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}/>
-          <EmployeeCardComponent name="Marilyn Adsit" phone="(780) 123-4567" email="adsit@outlook.ca" srcImg={"https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}/>
-          <EmployeeCardComponent name="James H." phone="(780) 123-4567" email="james@outlook.ca" srcImg={"https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}/>
+          <EmployeeCardComponent name="Boris Nedev" phone="(780) 123-4567" email="boris@outlook.ca" srcImg={"https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}/>
         </Row>
       </Container>
     </>
