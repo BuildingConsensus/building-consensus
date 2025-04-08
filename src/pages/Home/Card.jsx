24a85fb569ab974b875a089c20d25cb6c6036acb
Boris <bbnedev@gmail.com>
2025-04-08 22:27:09 +0000

    style: just reformatted the code with prettier

--- src/pages/Home/Card.jsx
+++ src/pages/Home/Card.jsx
@@ -1,15 +1,22 @@
-import Card from 'react-bootstrap/Card';
-import { Link } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
-import './Card.css';
+import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import "./Card.css";
 
 export function CardComponent({ title, description, link }) {
   return (
-    <Card style={{ width: '20rem' }} className='card'>
+    <Card style={{ width: "20rem" }} className="card">
       <Card.Body>
-        <Card.Title className='mb-3 text-center'>{title}</Card.Title>
-        <Card.Text className='text-muted'>{description}</Card.Text>
-        <Button as={Link} to={link} variant="primary" className='card-button stretched-link'>Learn more</Button>
+        <Card.Title className="mb-3 text-center">{title}</Card.Title>
+        <Card.Text className="text-muted">{description}</Card.Text>
+        <Button
+          as={Link}
+          to={link}
+          variant="primary"
+          className="card-button stretched-link"
+        >
+          Learn more
+        </Button>
       </Card.Body>
     </Card>
   );
