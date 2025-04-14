cb312198405dc6fdeae39d9e5eb4854b83278e5d
Boris <bbnedev@gmail.com>
2025-04-14 21:38:01 +0000

    chore: Changed the Logo to be the sunrise from the figma

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,12 @@
-import Image from 'react-bootstrap/Image';
-import sunset from '../assets/sunset cropped.jpg';
-import './Logo.css';
+import Image from "react-bootstrap/Image";
+import Sunrise from "../assets/Sunrise-cropped.png";
+import "./Logo.css";
+import { Container } from "react-bootstrap";
 
 export function Logo() {
   return (
-    <>
-      <Image src={sunset} alt="Sunset" className='sunset-image' fluid />
-    </>
+    <Container className=" d-flex justify-content-center bg-primary" fluid>
+      <Image src={Sunrise} alt="Sunrise" className="sunrise-image" />
+    </Container>
   );
 }
