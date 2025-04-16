56b02245e6b7dd7b828c8407a4704dbfa0428b6f
Boris <bbnedev@gmail.com>
2025-04-16 21:11:15 +0000

    chore: added 2 text boxes to the home page.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,20 +8,30 @@ import "./Home.css";
 
 export function Home() {
   return (
-    <Container fluid className="home-container pt-3">
+    <Container fluid className="home-container pt-3 d-grid">
       <h2 className="company-name">Building Consensus</h2>
       <p className="description">
         Mediations, Civil Disputes, <br /> and Elections Appeals
       </p>
-      <Textbox
-        title="What is Mediation?"
-        text="Mediation is a form of assisted negotiation 
-          where a neutral third party, the mediator, 
-          helps  disputing parties find a mutually agreeable resolution to their conflict. 
-          It's a structured process that allows parties to express their perspectives, explore solutions, 
-          and potentially reach a settlement that addresses  their needs."
-        variant="normal"
-      ></Textbox>
+      <Row className="centered justify-content-around my-5">
+        <Textbox
+          title="What is Mediation?"
+          text="Mediation is a form of assisted negotiation 
+            where a neutral third party, the mediator, 
+            helps  disputing parties find a mutually agreeable resolution to their conflict. 
+            It's a structured process that allows parties to express their perspectives, explore solutions, 
+            and potentially reach a settlement that addresses  their needs."
+          variant="normal"
+        ></Textbox>
+        <Textbox
+          title="Why Mediate?"
+          text="Mediation offers a more collaborative and less formal approach to resolving disputes than going to court. 
+          Mediation is typically quicker, more cost-effective, and results in mutually agreeable solutions. 
+          It allows parties to retain control over the outcome and tailor solutions  to their specific needs, 
+          fostering a more lasting resolution."
+          variant="normal"
+        ></Textbox>
+      </Row>
       <PhotoText
         title="Our Experience"
         text="We consist of three lawyers who bring a combined 50 years of legal and ADR resolution  experience. 
@@ -31,6 +41,20 @@ export function Home() {
           of natural justice and court procedures."
         variant="normal"
       />
+      <Row className="centered">
+        <Textbox
+          title={
+            <>
+              Want to know more?
+              <br />
+              Check out the cards below
+            </>
+          }
+          text=""
+          variant="nobar"
+        ></Textbox>
+      </Row>
+
       <Row className="centered mb-3">
         <CardComponent
           title="Indigenous Election Appeals"
