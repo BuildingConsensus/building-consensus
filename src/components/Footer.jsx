ff09efec9aff172c331cfd5e762b71eb11ff0a1a
jjadsit <adsit@ualberta.ca>
2025-04-26 09:14:47 -0600

    chore(footer): contact us filled in

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,29 +23,40 @@ export function Footer() {
         <Col>
           <h5 className="text-white"> OFFICE LOCATIONS </h5>
           <Stack className="mx-5 pt-2" direction="horizontal" gap={0}>
-            <p className="text-white px-2"> Saskatoon Office: <br/> <br/> <br/> <br/> </p>
+            <p className="text-white px-2"> Saskatoon Office: <br /> <br /> <br /> <br /> </p>
             <p className="text-white">
-              210 - 335 Packham Ave. <br/>
-              Saskatoon, Saskatchewan S7N 4S1. <br/>
-              PHONE: 306-373-8511. <br/>
+              210 - 335 Packham Ave. <br />
+              Saskatoon, Saskatchewan S7N 4S1. <br />
+              PHONE: 306-373-8511. <br />
               FAX: 306-373-8510
             </p>
           </Stack>
           <Stack className="mx-5 pt-2" direction="horizontal" gap={0}>
-            <p className="text-white px-2"> Edmonton Office: <br/> <br/> <br/> <br/> </p>
+            <p className="text-white px-2"> Edmonton Office: <br /> <br /> <br /> <br /> </p>
             <p className="text-white">
-              85902 - 335 Packham Ave. <br/>
-              Edmonton, Alberta T7A 3B2. <br/>
-              PHONE: 306-373-8511. <br/>
+              85902 - 335 Packham Ave. <br />
+              Edmonton, Alberta T7A 3B2. <br />
+              PHONE: 306-373-8511. <br />
               FAX: 306-373-8510
             </p>
           </Stack>
         </Col>
         <Col>
-          <p className="text-white"> Item 1 </p>
-          <p className="text-white"> Item 2 </p>
-          <p className="text-white"> Item 3 </p>
-          <p className="text-white"> Item 4 </p>
+          <h5 className="text-white"> CONTACT US </h5>
+          <Stack className="mx-5 pt-2" direction="horizontal" gap={0}>
+            <p className="text-white px-2"> Marilyn P. Adsit:<br /> <br /></p>
+            <p className="text-white">
+              780-123-4567 <br />
+              adsit@outlook.ca
+            </p>
+          </Stack>
+          <Stack className="mx-5 pt-2" direction="horizontal" gap={0}>
+            <p className="text-white px-2"> Partner 2:<br /> <br /></p>
+            <p className="text-white">
+              780-123-4567 <br />
+              adsit@outlook.ca
+            </p>
+          </Stack>
         </Col>
       </Row>
     </Container>
