2c7ce744d50c0b8158b938ac7fc58799acd16f67
jjadsit <adsit@ualberta.ca>
2025-04-26 09:42:09 -0600

    refactor(footer): remove Stack, use Row & Col

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { Stack, Container, Col, Row, Image } from "react-bootstrap";
+import { Container, Col, Row, Image } from "react-bootstrap";
 import Facebook from "../assets/Facebook.png";
 import "../App.css"
 
@@ -8,41 +8,57 @@ export function Footer() {
       <Row className="mx-5 pt-2">
         <Col>
           <h5 className="text-white"> OFFICE LOCATIONS </h5>
-          <Stack direction="horizontal" gap={0}>
-            <p className="text-white px-2"> Saskatoon Office: <br /> <br /> <br /> <br /> </p>
-            <p className="text-white">
-              210 - 335 Packham Ave. <br />
-              Saskatoon, Saskatchewan S7N 4S1. <br />
-              PHONE: 306-373-8511. <br />
-              FAX: 306-373-8510
-            </p>
-          </Stack>
-          <Stack direction="horizontal" gap={0}>
-            <p className="text-white px-2"> Edmonton Office: <br /> <br /> <br /> <br /> </p>
-            <p className="text-white">
-              85902 - 335 Packham Ave. <br />
-              Edmonton, Alberta T7A 3B2. <br />
-              PHONE: 306-373-8511. <br />
-              FAX: 306-373-8510
-            </p>
-          </Stack>
+          <Row>
+            <Col md="auto">
+              <p className="text-white px-2"> Saskatoon Office:</p>
+            </Col>
+            <Col>
+              <p className="text-white">
+                210 - 335 Packham Ave. <br />
+                Saskatoon, Saskatchewan S7N 4S1. <br />
+                PHONE: 306-373-8511. <br />
+                FAX: 306-373-8510
+              </p>
+            </Col>
+          </Row>
+          <Row>
+            <Col md="auto">
+              <p className="text-white px-2"> Edmonton Office:</p>
+            </Col>
+            <Col>
+              <p className="text-white">
+                85902 - 335 Packham Ave. <br />
+                Edmonton, Alberta T7A 3B2. <br />
+                PHONE: 306-373-8511. <br />
+                FAX: 306-373-8510
+              </p>
+            </Col>
+          </Row>
         </Col>
         <Col>
           <h5 className="text-white"> CONTACT US </h5>
-          <Stack direction="horizontal" gap={0}>
-            <p className="text-white px-2"> Marilyn P. Adsit:<br /> <br /></p>
-            <p className="text-white">
-              780-123-4567 <br />
-              adsit@outlook.ca
-            </p>
-          </Stack>
-          <Stack direction="horizontal" gap={0}>
-            <p className="text-white px-2"> Partner 2:<br /> <br /></p>
-            <p className="text-white">
-              780-123-4567 <br />
-              adsit@outlook.ca
-            </p>
-          </Stack>
+          <Row>
+            <Col md="auto">
+              <p className="text-white px-2"> Marilyn P. Adsit:</p>
+            </Col>
+            <Col>
+              <p className="text-white">
+                780-123-4567 <br />
+                adsit@outlook.ca
+              </p>
+            </Col>
+          </Row>
+          <Row>
+            <Col md="auto">
+              <p className="text-white px-2"> Parter 2:</p>
+            </Col>
+            <Col>
+              <p className="text-white">
+                780-123-4567 <br />
+                adsit@outlook.ca
+              </p>
+            </Col>
+          </Row>
           {/* TODO: link to facebook page once created.*/}
           <a href="https://www.facebook.com">
             <Image src={Facebook} width="50px" height="50px" />
