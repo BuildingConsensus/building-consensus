dfee144dc355d2439e22248f19a20ccbfcd9e334
jjadsit <adsit@ualberta.ca>
2025-04-28 08:38:36 -0600

    chore: update Card design
    Changed color of button to tomato, set fixed height

--- src/pages/Home/Card.jsx
+++ src/pages/Home/Card.jsx
@@ -5,19 +5,19 @@ import "./Card.css";
 
 export function CardComponent({ title, description, link }) {
   return (
-    <Card style={{ width: "20rem" }} className="card">
+    <Card style={{ width: "20rem", height: "25rem" }} className="card">
       <Card.Body>
         <Card.Title className="mb-3 text-center">{title}</Card.Title>
         <Card.Text className="text-muted">{description}</Card.Text>
-        <Button
-          as={Link}
-          to={link}
-          variant="primary"
-          className="card-button stretched-link"
-        >
-          Learn more
-        </Button>
       </Card.Body>
+      <Button
+        as={Link}
+        to={link}
+        variant="primary"
+        className="card-button stretched-link mb-2"
+      >
+        Learn more
+      </Button>
     </Card>
   );
 }
