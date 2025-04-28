80a0c42b03bbed743aef0e18879d99205d65a3ee
jjadsit <adsit@ualberta.ca>
2025-04-28 09:26:26 -0600

    chore: removed 'What is Mediation' from homepage
    Layout of Homepage matches figma and has:
    - Logo & navbar
    - Our Vision
    - 4 cards
    - Employees
    - Footer

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,6 @@
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import { CardComponent } from "./Card";
 import { EmployeeCardComponent } from "./EmployeeCard";
-import { PhotoText } from "../../components/PhotoText";
 import { Textbox } from "../../components/Textbox";
 import Image from "react-bootstrap/Image";
 import topImg from "../../assets/skyline-cropped.jpg";
@@ -11,17 +10,18 @@ import "./Home.css";
 export function Home() {
   return (
     <Container fluid className="home-container d-grid gap-5 p-0">
-      <Container className="justify-content-center d-flex border border-dark p-0 border-2 position-relative bg-secondary d-none d-md-flex">
+      {/* Our Vision Container */ }
+      <Container fluid="xl" className="justify-content-center d-flex border border-dark p-0 border-2 position-relative bg-secondary d-none d-md-flex">
         <Image src={topImg} className="bg-image" fluid></Image>
-        <Container className="d-flex img-text flex-column">
+        <Container fluid className="d-flex img-text flex-column">
           <h2 className="display-1 text-start border-bottom border-dark border-4 pt-xl-5 mx-5 mt-xxl-5 my-lg-4 mb-md-2 mb-sm-1">
             Our Vision
           </h2>
           <p className="text-center lead fs-2 my-xl-5 my-lg-4 mb-md-2 mb-sm-1">
-            We aim to provide independent impartial and substantial consultation
+            Our mission is to provide independent impartial and substantial consultation
           </p>
           <p className="text-center lead fs-2 mb-xl-5 mb-lg-4 mb-md-2 mb-sm-1">
-            Our mission is to resolve disputes by consensus building in
+            We aim to resolve disputes by consensus building in
             communities
           </p>
           <p className="text-center lead fs-2">
@@ -29,64 +29,45 @@ export function Home() {
           </p>
         </Container>
       </Container>
-      <Row className="centered justify-content-around my-5">
+
+      <Row className="centered">
         <Textbox
-          title="What is Mediation?"
-          text="Mediation is a form of assisted negotiation 
-            where a neutral third party, the mediator, 
-            helps  disputing parties find a mutually agreeable resolution to their conflict. 
-            It's a structured process that allows parties to express their perspectives, explore solutions, 
-            and potentially reach a settlement that addresses  their needs."
-          variant="normal"
-        ></Textbox>
-        <Textbox
-          title="Why Mediate?"
-          text="Mediation offers a more collaborative and less formal approach to resolving disputes than going to court. 
-          Mediation is typically quicker, more cost-effective, and results in mutually agreeable solutions. 
-          It allows parties to retain control over the outcome and tailor solutions  to their specific needs, 
-          fostering a more lasting resolution."
-          variant="normal"
-        ></Textbox>
-      </Row>
-      <PhotoText
-        title="Our Experience"
-        text="We consist of three lawyers who bring a combined 50 years of legal and ADR resolution  experience. 
-          All three have sat before professional tribunals such as the Indian Residential School tribunal,
-          the Human Rights Commission, Rentals Board, and First Nation tribunals.
-          We all have extensive court and litigation experience paired with a complex understanding of principles 
-          of natural justice and court procedures."
-        variant="normal"
-      />
-      <Row className="centered mt-5">
-        <Textbox
-          title={
-            <>
-              Want to know more?
-              <br />
-              Check out the cards below
-            </>
-          }
+          title="Our Services"
           text=""
           variant="nobar"
         ></Textbox>
       </Row>
 
-      <Row className="centered mb-5">
-        <CardComponent
-          title="Indigenous Election Appeals"
-          description="Resolve band and council elections appeals in 6 months or less."
-          link="/ElectionAppeals"
-        />
-        <CardComponent
-          title="Family Mediation"
-          description="Family law and court ordered mediations."
-          link="/FamilyMediation"
-        />
-        <CardComponent
-          title="Employment Mediation"
-          description="Resolve workplace disputes and grievances."
-          link="/EmploymentMediation"
-        />
+      {/* 4 Cards */ }
+      <Row>
+        {/* style={{justifyContent: 'right'}}
+              className="justify-content-end"
+              className="justify-content-evenly"
+        */}
+        <Col className="justify-content-end">
+          <CardComponent
+            title="Election Appeals"
+            description="Resolve band and council elections appeals in 6 months or less."
+            link="/ElectionAppeals"
+          />
+          <CardComponent
+            title="Civil Disputes"
+            description="Resolve workplace disputes and grievances."
+            link="/EmploymentMediation"
+          />
+        </Col>
+        <Col>
+          <CardComponent
+            title="Family Mediation"
+            description="Family law and court ordered mediations."
+            link="/FamilyMediation"
+          />
+          <CardComponent
+            title="Estate Services"
+            description="Content"
+            link="/FamilyMediation"
+          />
+        </Col>
       </Row>
       <Row className="flex-nowrap overflow-auto employee-row mt-5">
         <EmployeeCardComponent
