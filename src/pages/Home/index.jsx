cace130ff491a8e56448ced3916ece2bd4dc4b51
jjadsit <adsit@ualberta.ca>
2025-05-12 08:58:29 -0600

    chore: changed wording on Our Vision

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -18,15 +18,15 @@ export function Home() {
     return () => window.removeEventListener("resize", updateHeight);
   }, []);
 
-  const mediation_card_text = <>Resolve your legal conflict peacefully through respectful negotiation <br/><br/> We facilitate conversations and guides all parties toward a mutually acceptable agreement</>;
-  const elections_card_text = <>Is there an dispute with your Band or Council election? <br/> <br/> Our impartial Tribunal services can resolve your election in six months or less</>;
-  const disputes_card_text = <>Do you have a civil dispute? <br/> <br/> Resolve it confidentially and faster than in court, through our mediation, arbitration and tribunal services. </>;
-  const estates_card_text = <>Secure your family's future with confidence—we specialize in  estate planning services, including wills, trusts, power of attorney, and probate guidance.<br/> <br/>Let us help you protect what matters most. </>;
+  const mediation_card_text = <>Resolve your legal conflict peacefully through respectful negotiation <br /><br /> We facilitate conversations and guides all parties toward a mutually acceptable agreement</>;
+  const elections_card_text = <>Is there an dispute with your Band or Council election? <br /> <br /> Our impartial Tribunal services can resolve your election in six months or less</>;
+  const disputes_card_text = <>Do you have a civil dispute? <br /> <br /> Resolve it confidentially and faster than in court, through our mediation, arbitration and tribunal services. </>;
+  const estates_card_text = <>Secure your family's future with confidence—we specialize in  estate planning services, including wills, trusts, power of attorney, and probate guidance.<br /> <br />Let us help you protect what matters most. </>;
   return (
     <Container fluid className="home-container d-grid p-0">
       {/* Our Vision Container */}
       <Container fluid className="justify-content-center border border-dark p-0 border-2 position-relative bg-secondary d-none d-md-flex">
-        <Image src={topImg} style={{height: height}} className="bg-image"/>
+        <Image src={topImg} style={{ height: height }} className="bg-image" />
         <Container fluid ref={ourVisionRef} className="img-text">
           <h2 className="display-5 text-start border-bottom border-dark border-4 mx-5 pt-2">
             Our Vision
@@ -35,8 +35,7 @@ export function Home() {
             Our mission is to provide independent impartial and substantial consultation
           </p>
           <p className="text-center lead fs-3 py-1 my-1">
-            We aim to resolve disputes by consensus building in
-            communities
+            We aim to make our services accessible to all communities
           </p>
           <p className="text-center lead fs-3 py-1 my-1">
             Restoring relationships and keeping the peace
@@ -53,28 +52,28 @@ export function Home() {
       </Row>
 
       {/* 4 Cards */}
-        <Row className="centered">
-          <CardComponent
-            title="Election Appeals"
-            description={elections_card_text}
-            link="/ElectionAppeals"
-          />
-          <CardComponent
-            title="Civil Disputes"
-            description={disputes_card_text}
-            link="/EmploymentMediation"
-          />
-          <CardComponent
-            title="Family Mediation"
-            description={mediation_card_text}
-            link="/FamilyMediation"
-          />
-          <CardComponent
-            title="Estate Services"
-            description={estates_card_text}
-            link="/EstateServices"
-          />
-        </Row>
+      <Row className="centered">
+        <CardComponent
+          title="Election Appeals"
+          description={elections_card_text}
+          link="/ElectionAppeals"
+        />
+        <CardComponent
+          title="Civil Disputes"
+          description={disputes_card_text}
+          link="/EmploymentMediation"
+        />
+        <CardComponent
+          title="Family Mediation"
+          description={mediation_card_text}
+          link="/FamilyMediation"
+        />
+        <CardComponent
+          title="Estate Services"
+          description={estates_card_text}
+          link="/EstateServices"
+        />
+      </Row>
 
       <Container
         className="d-flex align-items-center flex-column px-0 py-5 bg-default "
