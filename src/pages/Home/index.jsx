d171626bb22fa86b56cbab6eb264b6ce5f33cf7a
Boris <bbnedev@gmail.com>
2025-05-13 19:34:41 +0000

    style: prettier restyled some text

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,25 +6,55 @@ import { Textbox } from "../../components/Textbox";
 import { CardComponent } from "./Card";
 
 export function Home() {
-  const mediation_card_text = <>Resolve your legal conflict peacefully through respectful negotiation <br/><br/> We facilitate conversations and guides all parties toward a mutually acceptable agreement</>;
-  const elections_card_text = <>Is there an dispute with your Band or Council election? <br/> <br/> Our impartial Tribunal services can resolve your election in six months or less</>;
-  const disputes_card_text = <>Do you have a civil dispute? <br/> <br/> Resolve it confidentially and faster than in court, through our mediation, arbitration and tribunal services. </>;
-  const estates_card_text = <>Secure your family's future with confidence—we specialize in  estate planning services, including wills, trusts, power of attorney, and probate guidance.<br/> <br/>Let us help you protect what matters most. </>;
+  const mediation_card_text = (
+    <>
+      Resolve your legal conflict peacefully through respectful negotiation{" "}
+      <br />
+      <br /> We facilitate conversations and guides all parties toward a
+      mutually acceptable agreement
+    </>
+  );
+  const elections_card_text = (
+    <>
+      Is there an dispute with your Band or Council election? <br /> <br /> Our
+      impartial Tribunal services can resolve your election in six months or
+      less
+    </>
+  );
+  const disputes_card_text = (
+    <>
+      Do you have a civil dispute? <br /> <br /> Resolve it confidentially and
+      faster than in court, through our mediation, arbitration and tribunal
+      services.{" "}
+    </>
+  );
+  const estates_card_text = (
+    <>
+      Secure your family's future with confidence—we specialize in estate
+      planning services, including wills, trusts, power of attorney, and probate
+      guidance.
+      <br /> <br />
+      Let us help you protect what matters most.{" "}
+    </>
+  );
   return (
     <Container fluid className="home-container d-grid gap-2 p-0">
       {/* Our Vision Container */}
-      <Container fluid="xl" className="justify-content-center d-flex border border-dark p-0 border-2 position-relative bg-secondary d-none d-md-flex">
+      <Container
+        fluid="xl"
+        className="justify-content-center d-flex border border-dark p-0 border-2 position-relative bg-secondary d-none d-md-flex"
+      >
         <Image src={topImg} className="bg-image" fluid></Image>
         <Container fluid className="d-flex img-text flex-column">
           <h2 className="display-1 text-start border-bottom border-dark border-4 pt-xl-5 mx-5 mt-xxl-5 my-lg-4 mb-md-2 mb-sm-1">
             Our Vision
           </h2>
           <p className="text-center lead fs-2 my-xl-5 my-lg-4 mb-md-2 mb-sm-1">
-            Our mission is to provide independent impartial and substantial consultation
+            Our mission is to provide independent impartial and substantial
+            consultation
           </p>
           <p className="text-center lead fs-2 mb-xl-5 mb-lg-4 mb-md-2 mb-sm-1">
-            We aim to resolve disputes by consensus building in
-            communities
+            We aim to resolve disputes by consensus building in communities
           </p>
           <p className="text-center lead fs-2">
             Restoring relationships and keeping the peace
@@ -33,11 +63,7 @@ export function Home() {
       </Container>
       <Container fluid>
         <Row className="centered">
-          <Textbox
-            title="Our Services"
-            text=""
-            variant="nobar"
-          ></Textbox>
+          <Textbox title="Our Services" text="" variant="nobar"></Textbox>
         </Row>
       </Container>
 
@@ -71,7 +97,6 @@ export function Home() {
         </Col>
       </Container>
 
-
       <Container
         className="d-flex align-items-center flex-column px-0 py-5 bg-default "
         fluid
@@ -90,8 +115,7 @@ export function Home() {
               />
             </Container>
           </Carousel.Item>
-          <Carousel.Item>
-          </Carousel.Item>
+          <Carousel.Item></Carousel.Item>
         </Carousel>
       </Container>
     </Container>
