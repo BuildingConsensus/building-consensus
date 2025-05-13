9d246de1e7bc50ae22f6deb5b612559800f584b8
Boris <bbnedev@gmail.com>
2025-05-13 19:18:29 +0000

    chore: made elections page responsive & added image for tribunal PhotoText component

--- src/pages/ElectionAppeals/index.jsx
+++ src/pages/ElectionAppeals/index.jsx
@@ -6,6 +6,7 @@ import { Col, Container, Row } from "react-bootstrap";
 // --------------------------------------------------------
 import Steps from "../../assets/Steps Diagram.png";
 // --------------------------------------------------------
+import Tribunal from "../../assets/tribunal.jpg";
 
 import CircleNumber1 from "../../assets/CircleNumber1.png";
 import CircleNumber2 from "../../assets/CircleNumber2.png";
@@ -16,18 +17,18 @@ import { PhotoText } from "../../components/PhotoText";
 export function ElectionAppeals() {
   return (
     <Container fluid>
-      <Row className="p-5 mb-5">
-        <Col className="border rounded-3 border-dark">
+      <Row className="p-lg-5 p-3 mb-5">
+        <Col xs="auto" lg={6} className="border rounded-3 border-dark mb-5">
           <h1 className="text-center fs-2 fw-bold">
             We aim to resolve disputes by consensus building in communities with
             a simplified process:
           </h1>
           <Row className="align-items-center">
-            <Col>
+            <Col sm="auto">
               <Image src={CircleNumber1} />
             </Col>
 
-            <Col sm={10}>
+            <Col>
               <h1>Step 1</h1>
               <p>
                 Within the time limit as set out in your election code, an
@@ -38,11 +39,11 @@ export function ElectionAppeals() {
             </Col>
           </Row>
           <Row className="align-items-center">
-            <Col>
+            <Col sm="auto">
               <Image src={CircleNumber2} />
             </Col>
 
-            <Col sm={10}>
+            <Col>
               <h1>Step 2</h1>
               <p>
                 The community election code specifies the appeal procedure may
@@ -53,11 +54,11 @@ export function ElectionAppeals() {
             </Col>
           </Row>
           <Row className="align-items-center">
-            <Col>
+            <Col sm="auto">
               <Image src={CircleNumber3} />
             </Col>
 
-            <Col sm={10}>
+            <Col>
               <h1>Step 3</h1>
               <p>
                 There is an option to negotiate or mediate the disagreement with
@@ -66,11 +67,11 @@ export function ElectionAppeals() {
             </Col>
           </Row>
           <Row className="align-items-center">
-            <Col>
+            <Col sm="auto">
               <Image src={CircleNumber4} />
             </Col>
 
-            <Col sm={10}>
+            <Col>
               <h1>Step 4</h1>
               <p>
                 Contact Building Consensus with the appeal document and our
@@ -82,10 +83,11 @@ export function ElectionAppeals() {
           </Row>
         </Col>
         <Col className="align-self-center text-center">
-          <Image src={Steps} />
+          <Image src={Steps} fluid />
         </Col>
       </Row>
       <PhotoText
+        photoURL={Tribunal}
         title="What is a Tribunal?"
         text="Tribunals are specialized bodies that resolve disputes by making decisions based on the evidence presented. 
         They operate independently and impartially, ensuring that decisions are made in a fair manner. 
